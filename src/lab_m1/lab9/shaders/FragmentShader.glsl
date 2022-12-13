ce8e02a43c171e648e5661d6fbef8c5586393be8
BogSav <bogdansava59@yahoo.com>
2022-12-13 08:54:04 +0200

    All

--- src/lab_m1/lab9/shaders/FragmentShader.glsl
+++ src/lab_m1/lab9/shaders/FragmentShader.glsl
@@ -7,6 +7,7 @@ in vec2 texcoord;
 uniform sampler2D texture_1;
 uniform sampler2D texture_2;
 // TODO(student): Declare various other uniforms
+uniform int useMix;
 
 // Output
 layout(location = 0) out vec4 out_color;
@@ -15,6 +16,17 @@ layout(location = 0) out vec4 out_color;
 void main()
 {
     // TODO(student): Calculate the out_color using the texture2D() function.
-    out_color = vec4(1);
+	vec4 color = texture2D(texture_1, texcoord);
+	vec4 color2 = texture2D(texture_2, texcoord);
+
+	if(color.w < 0.5f)
+	{
+		discard;
+	}
+
+	if(useMix == 1)
+		out_color = mix(color, color2, 0.5);
+	else
+		out_color = color;
 
 }
