07830ebe82341d5a69ea5b54b01af345a76f61aa
Bogdan <bogdansava59@yahoo.com>
2023-11-24 09:16:18 +0200

    Applied median filter

--- src/lab_m2/lab8/shaders/FragmentShader.glsl
+++ src/lab_m2/lab8/shaders/FragmentShader.glsl
@@ -40,6 +40,50 @@ vec4 blur(int blurRadius)
     return sum / samples;
 }
 
+float getGrayValue(vec4 color)
+{
+    return 0.21 * color.r + 0.71 * color.g + 0.07 * color.b; 
+}
+
+void bubbleSort(inout vec4 arr[9], int n)
+{
+    int i, j;
+    bool swapped;
+
+    for (i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (j = 0; j < n - i - 1; j++) {
+            if (getGrayValue(arr[j]) > getGrayValue(arr[j + 1])){
+                vec4 aux = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = aux;
+                swapped = true;
+            }
+        }
+
+        if (swapped == false)
+            break;
+    }
+}
+
+vec4 median()
+{
+    vec2 texelSize = 1.0f / screenSize;
+    vec4 sum = vec4(0);
+    vec4 a[9];
+    for(int i = 0; i <= 2; i++)
+    {
+        for(int j = 0; j <= 2; j++)
+        {
+            a[i * 3 + j] = texture(textureImage, textureCoord + vec2(i - 1, j - 1) * texelSize);
+        }
+    }
+
+    bubbleSort(a, 9);
+
+    return a[4];
+}
+
 
 void main()
 {
@@ -54,6 +98,12 @@ void main()
         case 2:
         {
             out_color = blur(3);
+            break;
+        }
+
+        case 3:
+        {
+            out_color = median();
             break;
         }
 
